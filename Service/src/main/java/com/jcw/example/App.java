b7b7fecba22cced075df0605687f39f0970658b6
jiangchengwei <jiangchengweijcw@qq.com>
2017-06-10 13:18:52 +0800

    Initial commit

---
+++ Service/src/main/java/com/jcw/example/App.java
@@ -0,0 +1,13 @@
+package com.jcw.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
